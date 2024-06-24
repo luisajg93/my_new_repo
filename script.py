d50925e323db48b9eee1627c5dcbc5d0fcb2505c
Luis Jimenez <lbtojg@gmail.com>
2024-06-24 00:02:37 -0600

    se agregaron los settings

--- script.py
+++ script.py
@@ -1,5 +1,14 @@
-print('Comienza la práctica en VS Code')
+import numpy
+import matplotlib.pyplot as plt
+import math
+import pandas as pd
 
 import os
-
 print(f'Mi directorio de trabajo es {os.getcwd()}')
+
+x = 10
+
+if x % 2 == 0:
+    print(f'{x} es un número par')
+else:
+    print(f'{x} es un número impar')
